df7975ed2d854cd36c5b306bc580a347df443b38
Marcia Satie <111194629+MarciaSatie@users.noreply.github.com>
2026-07-26 16:11:13 +0100

    changing tabs to page

--- src/components/recipesDetail/index.tsx
+++ src/components/recipesDetail/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+
 import { useQueryRecipeByID } from '../../hooks/useRecipes'; 
 import Spinner from "../../components/spinner";
 import { Typography,Container,Box,Paper ,Chip, Divider, CardMedia, Tabs, Tab, Card} from '@mui/material';
@@ -11,11 +11,6 @@ import myLogo from '../../assets/cook-hat.png';
 type RecipesDetailProps = { id?: string };
 export default function RecipesDetail({ id }: RecipesDetailProps){
 
-  const [tabValue, setTabValue] = useState<number>(0);
-
-  const handleTabChange = (_event: React.SyntheticEvent, newValue: number) => {
-    setTabValue(newValue);
-  };
   
   const {recipe, isLoading, isError, error} = useQueryRecipeByID(id!);
   if (!recipe) return <div>No recipe found</div>;
@@ -26,110 +21,79 @@ export default function RecipesDetail({ id }: RecipesDetailProps){
   const instructionSteps = utils.recipeInstructionsFormatter(recipe);
 
   return (
-      <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', }}>
+    <Box>
+      {/* Recipe Title */}
+      <Typography color="primary" variant="h4" component="h1" gutterBottom>
+      <Box component="img" src={myLogo} alt="Logo" sx={{ width: 50, height: 50, marginRight: 2 }} />
+        {recipe.strMeal}
+      </Typography>
 
-        {/* Main Content Area */}
-        {/* Container limits maximum width and centers content automatically */}
-        <Container component="main" maxWidth="md" sx={{ mt: 4, mb: 4, flexGrow: 1 }}>
-          {/* Paper MUI adds elevation/ dropshadow */}
-          <Paper elevation={3} sx={{ p: 4, borderRadius: 2 }}>
-
-            {/* Tabs switch between ingredients and instructions */}
-            <Tabs
-              value={tabValue}
-              onChange={handleTabChange}
-              variant="fullWidth"
-              sx={{ mb: 2, borderBottom: 1, borderColor: 'divider' }}
-            >
-              <Tab label="Recipe" />  {/* Index 0 */}
-              <Tab label="Reviews" />  {/* Index 1 */}
-            </Tabs>
-
-            {tabValue === 0 && (
-              <Box>
-                {/* Recipe Title */}
-                <Typography color="primary" variant="h4" component="h1" gutterBottom>
-                <Box component="img" src={myLogo} alt="Logo" sx={{ width: 50, height: 50, marginRight: 2 }} />
-                  {recipe.strMeal}
-                </Typography>
-
-                {/* Recipe Image */}
-                <Box   sx={{
-                  gap: 1,
-                  mb: 2,
-                  backgroundImage: `url(${bgImage})`,
-                  backgroundRepeat: "repeat",
-                  backgroundPosition: "top left",
-                  backgroundSize: "auto",
-                  p: 0,
-                }}>
-                  {showVideoIfExistsOrImage(recipe.strMealThumb, recipe.strYoutube!)}
-                </Box>
-                {/* Recipe Category, Area, Country and Tags*/}
-                <Box sx={{ display: 'flex', gap: 1, mb: 2 }}>
-                  <Chip label={recipe.strCategory} color="primary" size="small" />
-                  <Chip label={recipe.strArea} color="secondary" variant="outlined" size="small" />
-                  <Chip label={recipe.strCountry}/>
-                  <Chip label={recipe.strTags} color="secondary" variant="outlined" size="small"/>
-                </Box>
-                
-                <Divider color="yellow" sx={{m:2,}}></Divider>
-                {/* Ingrevient List */}
-                  <Box component="ul" sx={{ pl: 2, mt: 0, mb: 0 }}>
-                    <Typography variant="subtitle1" sx={{ fontSize: 25 ,fontWeight: 'bold', mb: 1 }}>
-                    <PlaylistAddCheckCircleTwoToneIcon color="secondary">
-                    </PlaylistAddCheckCircleTwoToneIcon> 
-                      Ingredients List
-                    </Typography>
-                  
-                    <ExpandableSection>
-                      {recipe.ingredientsList.map((item, index:number) => (
-                        <Box component="li" key={index} sx={{ mb: 0.5 }}>
-                          <Typography variant="body2">
-                            <strong>{item.measure}</strong> {item.name}
-                          </Typography>
-                        </Box>
-                      ))}
-                    </ExpandableSection>
-                </Box>
-                <Divider color="yellow" sx={{m:2,}}></Divider>
-                {/* Recipe Instructions */}
-                <Box component="ul" sx={{ pl: 2, mt: 0, mb: 0 }}>
-                    <Typography variant="subtitle1" sx={{ fontSize: 25 ,fontWeight: 'bold', mb: 1 }}>
-                    <PlaylistAddCheckCircleTwoToneIcon color="secondary">
-                    </PlaylistAddCheckCircleTwoToneIcon> 
-                      Instructions
-                    </Typography>
-                  
-                    <ExpandableSection>
-                    <Box component="ol" sx={{ pl: 0, mt: 0, mb: 0, listStyle: 'none' }}>
-                          {instructionSteps.map((step, index) => (
-                        <Box component="li" key={index} sx={{ mb: 1.5, display: 'flex', gap: 1 }}>
-                          <Typography variant="body2" sx={{ minWidth: 32, fontWeight: 'bold' }}>
-                            {/* Converts 1, 2, 3 into 01, 02, 03 so the instruction list uses padded numbering */}
-                            {String(index + 1).padStart(2, '0')}.
-                          </Typography>
-                          <Typography variant="body2" color="text.secondary" sx={{ lineHeight: 1.7 }}>
-                            {step}
-                          </Typography>
-                          </Box>
-                          ))}
-                      </Box>
-                    </ExpandableSection>
-                </Box>
-              </Box>
-            )}
-          
-          {tabValue === 1&& (
-            <Box>
-              In progress
-            </Box>
-          )}
-          </Paper>
-        </Container>
-
+      {/* Recipe Image */}
+      <Box   sx={{
+        gap: 1,
+        mb: 2,
+        backgroundImage: `url(${bgImage})`,
+        backgroundRepeat: "repeat",
+        backgroundPosition: "top left",
+        backgroundSize: "auto",
+        p: 0,
+      }}>
+        {showVideoIfExistsOrImage(recipe.strMealThumb, recipe.strYoutube!)}
       </Box>
-
+      {/* Recipe Category, Area, Country and Tags*/}
+      <Box sx={{ display: 'flex', gap: 1, mb: 2 }}>
+        <Chip label={recipe.strCategory} color="primary" size="small" />
+        <Chip label={recipe.strArea} color="secondary" variant="outlined" size="small" />
+        <Chip label={recipe.strCountry}/>
+        <Chip label={recipe.strTags} color="secondary" variant="outlined" size="small"/>
+      </Box>
+      
+      <Divider color="yellow" sx={{m:2,}}></Divider>
+      {/* Ingrevient List */}
+        <Box component="ul" sx={{ pl: 2, mt: 0, mb: 0 }}>
+          <Typography variant="subtitle1" sx={{ fontSize: 25 ,fontWeight: 'bold', mb: 1 }}>
+          <PlaylistAddCheckCircleTwoToneIcon color="secondary">
+          </PlaylistAddCheckCircleTwoToneIcon> 
+            Ingredients List
+          </Typography>
+        
+          <ExpandableSection>
+            {recipe.ingredientsList.map((item, index:number) => (
+              <Box component="li" key={index} sx={{ mb: 0.5 }}>
+                <Typography variant="body2">
+                  <strong>{item.measure}</strong> {item.name}
+                </Typography>
+              </Box>
+            ))}
+          </ExpandableSection>
+      </Box>
+      <Divider color="yellow" sx={{m:2,}}></Divider>
+      {/* Recipe Instructions */}
+      <Box component="ul" sx={{ pl: 2, mt: 0, mb: 0 }}>
+          <Typography variant="subtitle1" sx={{ fontSize: 25 ,fontWeight: 'bold', mb: 1 }}>
+          <PlaylistAddCheckCircleTwoToneIcon color="secondary">
+          </PlaylistAddCheckCircleTwoToneIcon> 
+            Instructions
+          </Typography>
+        
+          <ExpandableSection>
+          <Box component="ol" sx={{ pl: 0, mt: 0, mb: 0, listStyle: 'none' }}>
+                {instructionSteps.map((step, index) => (
+              <Box component="li" key={index} sx={{ mb: 1.5, display: 'flex', gap: 1 }}>
+                <Typography variant="body2" sx={{ minWidth: 32, fontWeight: 'bold' }}>
+                  {/* Converts 1, 2, 3 into 01, 02, 03 so the instruction list uses padded numbering */}
+                  {String(index + 1).padStart(2, '0')}.
+                </Typography>
+                <Typography variant="body2" color="text.secondary" sx={{ lineHeight: 1.7 }}>
+                  {step}
+                </Typography>
+                </Box>
+                ))}
+            </Box>
+          </ExpandableSection>
+      </Box>
+    </Box>
+           
   );
 
 }
--- src/pages/recipesAtoZPage.tsx
+++ src/pages/recipesAtoZPage.tsx
@@ -1,5 +1,6 @@
+import { useState } from 'react';
 import { useParams } from "react-router-dom";
-import { Box} from '@mui/material';
+import { Box,Tabs,Tab, Container, Paper} from '@mui/material';
 import bgImage2 from "../assets/backgorund_img2.png";
 import RecipesDetail from '../components/recipesDetail';
 
@@ -8,6 +9,11 @@ export default function FavoritesPage() {
   const { id } = useParams();
   console.log(`Recipe ID is ${id}`);
   
+  const [tabValue, setTabValue] = useState<number>(0);
+
+  const handleTabChange = (_event: React.SyntheticEvent, newValue: number) => {
+    setTabValue(newValue);
+  };
 
 
   return (
@@ -21,10 +27,39 @@ export default function FavoritesPage() {
     > 
 
       <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', }}>
-        <RecipesDetail
-            id= {id}
-        />
-      </Box>
+
+      {/* Main Content Area */}
+      {/* Container limits maximum width and centers content automatically */}
+      <Container component="main" maxWidth="md" sx={{ mt: 4, mb: 4, flexGrow: 1 }}>
+        {/* Paper MUI adds elevation/ dropshadow */}
+        <Paper elevation={3} sx={{ p: 4, borderRadius: 2 }}>
+
+          {/* Tabs switch between ingredients and instructions */}
+          <Tabs
+            value={tabValue}
+            onChange={handleTabChange}
+            variant="fullWidth"
+            sx={{ mb: 2, borderBottom: 1, borderColor: 'divider' }}
+          >
+            <Tab label="Recipe" />  {/* Index 0 */}
+            <Tab label="Reviews" />  {/* Index 1 */}
+          </Tabs>
+
+          {tabValue === 0 && (
+            <RecipesDetail
+                id={id}
+            />
+          )}
+        
+        {tabValue === 1&& (
+          <Box>
+            In progress
+          </Box>
+        )}
+        </Paper>
+      </Container>
+
+    </Box>
   
   </Box>
   );
